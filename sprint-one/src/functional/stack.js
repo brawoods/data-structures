dbf37dd1c5b36063425430c206bc90c0bbda176f
Brandon Woods <woods.brandon.m@gmail.com>
2023-02-08 14:46:23 -0800

    Pass tests fro functional pattern stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,28 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // identify lastKey in storage
+    let lastKey = Object.keys(storage).length - 1;
+    // set new value at index of lastKey + 1
+    storage[lastKey + 1] = value;
+    return storage.length;
   };
 
   someInstance.pop = function() {
+    // identify lastKey in storage
+    let lastKey = Object.keys(storage).length - 1;
+    // store last element in variable
+    let lastElement = storage[lastKey];
+    // remove value from object
+    delete storage[lastKey];
+    // return storage at lastKey
+    return lastElement;
   };
 
   someInstance.size = function() {
+    //set length =  use Object.keys to get a number collection of the keys
+    // return length
+    return Object.keys(storage).length;
   };
 
   return someInstance;
